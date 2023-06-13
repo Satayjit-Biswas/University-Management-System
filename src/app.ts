009797aaebc8ba663f2f05780b02d2bb9c7fd17f
Satayjit-Biswas <satayjitbiswas2@gmail.com>
2023-06-13 21:09:23 +0600

    setUp ESlint Prettier and husky

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
 
-//use cors
-app.use(cors());
+// use cors
+app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //welcome route
-app.get("/", (req: Request, res: Response) => {
-    res.json("Welcome to Auth Service!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.json('Welcome to Auth Service!')
+})
 
-export default app;
+export default app
